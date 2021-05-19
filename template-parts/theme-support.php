1d6da033e79d3ef8fde2da25e821f8c8f28ae77e
ketul8621 <ketul.solanki@pay1.in>
2021-05-19 17:19:06 +0530

    Add theme support for custom logo

--- template-parts/theme-support.php
+++ template-parts/theme-support.php
@@ -17,3 +17,23 @@ function df_register_nav_menu() {
 
 // Register df_register_nav_menu with after_setup_theme hook.
 add_action( 'after_setup_theme', 'df_register_nav_menu' );
+
+/**
+ * Option to add custom logo
+ *
+ * @return void
+ */
+function df_custom_logo_setup() {
+	$defaults = array(
+		'height'               => 100,
+		'width'                => 200,
+		'flex-height'          => true,
+		'flex-width'           => true,
+		'unlink-homepage-logo' => true,
+	);
+
+	add_theme_support( 'custom-logo', $defaults );
+}
+
+// Register df_custom_logo_setup with after_setup_theme hook.
+add_action( 'after_setup_theme', 'df_custom_logo_setup' );
