4980e2276fb49a39dc335138d699251693431f70
ketul8621 <ketul.solanki@pay1.in>
2021-05-19 17:17:32 +0530

    Register and enqueue design.css file

--- template-parts/enqueue.php
+++ template-parts/enqueue.php
@@ -13,16 +13,19 @@
 function df_enqueue_scripts() {
 
 	// Register styles.
-	wp_register_style( 'style-css', get_stylesheet_uri(), [], '1.0', 'all' );
-	wp_register_style( 'bootstrap-css', get_template_directory_uri() . '/assets/src/library/bootstrap.min.css', [], false, 'all' );
+	wp_register_style( 'style-css', get_stylesheet_uri(), array(), '1.0', 'all' );
+	wp_register_style( 'bootstrap-css', get_template_directory_uri() . '/assets/src/library/css/bootstrap.min.css', array(), '5.6', 'all' );
+	wp_register_style( 'dsign', get_template_directory_uri() . '/assets/src/library/css/design.css', array(), '1.0', 'all' );
+
 
 	// Register scripts.
-	wp_register_script( 'main-js', get_template_directory_uri() . '/assets/main.js', [], '1.0', true );
-	wp_register_script( 'bootstrap-js', get_template_directory_uri() . '/assets/src/library/bootstrap.min.js', [ 'jquery' ], false, true );
+	wp_register_script( 'main-js', get_template_directory_uri() . '/assets/src/library/js/main.js', array(), '1.0', true );
+	wp_register_script( 'bootstrap-js', get_template_directory_uri() . '/assets/src/library/js/bootstrap.min.js', array( 'jquery' ), '5.6', true );
 
 	// Enqueue styles.
 	wp_enqueue_style( 'style-css' );
 	wp_enqueue_style( 'bootstrap-css' );
+	wp_enqueue_style( 'dsign' );
 
 	// Enqueue scripts.
 	wp_enqueue_script( 'main-js' );
