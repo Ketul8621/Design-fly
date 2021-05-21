516883a6857201a8417e2de4db0d00cf2cffbb0f
ketul8621 <ketul.solanki@pay1.in>
2021-05-21 11:58:44 +0530

    Add nav bar and carousel

--- header.php
+++ header.php
@@ -24,4 +24,56 @@ if ( function_exists( 'wp_body_open' ) ) {
 	wp_body_open();
 }
 ?>
-<header> Header </header>
+
+	<div id="top-header" class="container-fluid">
+		<header class="header-container">
+			<div class="nav-container nav-container-df">
+				<div class="df-logo">
+					<?php
+						if ( function_exists( 'the_custom_logo' ) ) {
+							the_custom_logo();
+						}
+					?>
+				</div>
+				<nav class="navbar navbar-default navbar-df">
+					<?php
+						wp_nav_menu( array(
+							'theme_location' => 'Header_menu',
+							'container'      => false,
+							'menu_class'     => 'nav navbar-nav df-nav',
+							'walker'         => new Walker_Nav_Primary(),
+						)
+						);
+					?>
+				</nav>
+
+				<div class="search">
+					<form action="#">
+						<input type="text" name="search">
+						<button><img src="<?php echo get_template_directory_uri() . '/assets/src/library/images/search-icon.png'; ?>" alt="s"></button>
+					</form>
+				</div>
+			</div>
+			<?php if ( is_home() ): ?>
+				<div id="carousel1" class="carousel carousel-dark slide df-carousal" data-bs-ride="carousel">
+					<div class="carousel-inner">
+						<div class="carousel-item active df" data-bs-interval="10000">
+							<img src="<?php echo get_template_directory_uri() . '/assets/src/library/images/home/slider-image.png'; ?>" class="d-block w-100" alt="...">
+							<div class="carousel-caption d-none d-md-block">
+								<p class="df-heading">Gearing up the ideas</p>
+								<p class="df-one-liner">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+							</div>
+						</div>
+					</div>
+					<!--<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+						<span class="visually-hidden">Previous</span>
+					</button>
+					<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+						<span class="visually-hidden">Next</span>
+					</button>-->
+				</div>
+			<?php endif; ?>
+		</header>
+	</div>
