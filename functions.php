6c95160ba19f9370479440a6244d020bf9fa5d6c
ketul8621 <ketul.solanki@pay1.in>
2021-05-28 11:15:29 +0530

    Add code to include footbar-widgets.php

--- functions.php
+++ functions.php
@@ -8,6 +8,7 @@
 require get_template_directory() . '/template-parts/enqueue.php';
 require get_template_directory() . '/template-parts/theme-support.php';
 require get_template_directory() . '/include/walker.php';
+require get_template_directory() . '/template-parts/footbar-widgets.php';
 
 /**
  * Add active class to the current menu item
@@ -16,7 +17,7 @@ require get_template_directory() . '/include/walker.php';
  * @param object $item
  * @return void
  */
-function special_nav_class( $classes, $item ) {
+function df_special_nav_class( $classes, $item ) {
 
 	if ( in_array( 'current-menu-item', $classes ) ) {
 
@@ -26,4 +27,37 @@ function special_nav_class( $classes, $item ) {
 }
 
 // Register the special_nav_class with nav_menu_css_class hook.
-add_filter( 'nav_menu_css_class', 'special_nav_class', 10, 2 );
+add_filter( 'nav_menu_css_class', 'df_special_nav_class', 10, 2 );
+
+function df_my_widget() {
+
+	register_sidebar( array(
+		'name'          => 'Sidebar',
+		'id'            => 'yohe-1',
+		'before_widget' => '<div class="chw-widget">',
+        'after_widget'  => '</div>',
+	) );
+}
+
+add_action( 'widgets_init', 'df_my_widget' );
+
+function universalSearch() {
+	register_sidebar( array(
+		'name'          => 'Footer 1',
+		'id'            => 'universal-search-field',
+		'before_widget' => '<div class="chw-widget">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<div class="footer-1-title">',
+		'after_title'   => '</div>',
+	) );
+
+		//register_sidebar( array(
+		//	'name'          => 'Universal Search1',
+		//	'id'            => 'universal-search-field-1',
+		//	'before_widget' => '<div class="chw-newsletter">',
+		//	'after_widget'  => '</div>',
+		//	'before_title'  => '<h2 class="chw-newsletter_title">',
+		//	'after_title'   => '</h2>',
+		//) );
+}
+add_action( 'widgets_init', 'universalSearch' );
