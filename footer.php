caff47e706e9d04c6df24d22949aee19b92dfdf5
ketul8621 <ketul.solanki@pay1.in>
2021-05-28 11:10:16 +0530

    Add footer

--- footer.php
+++ footer.php
@@ -8,7 +8,28 @@
 
 ?>
 
-<footer> Footer </footer>
+<footer>
+	<div class="line"></div>
+	<div class="row footer-row">
+		<div class="col-4 offset-2 footer-col">
+			<?php
+			if ( is_active_sidebar( 'universal-search-field' ) ) :
+				dynamic_sidebar( 'universal-search-field' );
+			endif;
+			?>
+		</div>
+
+		<div class="col-4 footer-col-1">
+			<?php
+			if ( is_active_sidebar( 'universal-search-field-1' ) ) :
+				dynamic_sidebar( 'universal-search-field-1' );
+			endif;
+			?>
+		</div>
+	</div>
+
+	<!--<div>displaying...</div>-->
+</footer>
 
 <?php wp_footer(); ?>
 </body>
