e186ae98645e82ae0df71d755f1be87bd37d07b1
ketul8621 <ketul.solanki@pay1.in>
2021-06-12 16:56:35 +0530

    Set the dynamic version for script file

--- template-parts/enqueue.php
+++ template-parts/enqueue.php
@@ -17,20 +17,25 @@ function df_enqueue_scripts() {
 	wp_register_style( 'bootstrap-css', get_template_directory_uri() . '/assets/src/library/css/bootstrap.min.css', array(), '5.6', 'all' );
 	wp_register_style( 'dsign', get_template_directory_uri() . '/assets/src/library/css/design.css', array(), '1.0', 'all' );
 
-
+	//print_r(filemtime( get_template_directory() . '/assets/src/library/js/main.js' ));
+	//wp_die();
 	// Register scripts.
-	wp_register_script( 'main-js', get_template_directory_uri() . '/assets/src/library/js/main.js', array(), '1.0', true );
-	wp_register_script( 'bootstrap-js', get_template_directory_uri() . '/assets/src/library/js/bootstrap.min.js', array( 'jquery' ), '5.6', true );
-
+	wp_enqueue_script( 'main-js', get_template_directory_uri() . '/assets/src/library/js/main.js', array(), filemtime( get_template_directory() . '/assets/src/library/js/main.js' ), true );
+	wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/assets/src/library/js/bootstrap.min.js', array( 'jquery' ), '5.6', true );
+	wp_enqueue_script( 'tryit', get_template_directory_uri() . '/tryit.js', array(), '1.1', true);
 	// Enqueue styles.
 	wp_enqueue_style( 'style-css' );
 	wp_enqueue_style( 'bootstrap-css' );
 	wp_enqueue_style( 'dsign' );
 
 	// Enqueue scripts.
-	wp_enqueue_script( 'main-js' );
-	wp_enqueue_script( 'bootstrap-js' );
+	//wp_enqueue_script( 'main-js' );
+	//wp_enqueue_script( 'bootstrap-js' );
 }
 
 // Register the df_enqueue_scripts with wp_enqueue_scripts hook.
 add_action( 'wp_enqueue_scripts', 'df_enqueue_scripts' );
+
+//function () {
+//	echo "testing time";
+//}
