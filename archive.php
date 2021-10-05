393da09a5190c19cdec0bd628851e3a6faef615c
ketul8621 <ketul.solanki@pay1.in>
2021-10-05 11:30:07 +0530

    Create file for archive page of date, author, tag/category

---
+++ archive.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Main template file
+ *
+ * @package Design-fly
+ */
+
+get_header();
+?>
+
+<div id="primary" class="content-area">
+	<main id="main-blog" class="site-main" role="main">
+		<div class="blog-body">
+
+			<div class="row">
+				<div class="col-6 blog-column">
+					<p class="title-phrase"><?php the_archive_title(); ?></p>
+				</div>
+			</div>
+
+			<div class="short-line"></div>
+
+			<?php
+
+			//$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+
+			//echo get_query_var( 'paged' );
+			//echo $paged;
+			//$args = array(
+			//	'post_type' => 'post',
+			//	'paged' => $paged,
+			//);
+
+
+			//$post_query = new WP_Query( $args );
+
+			if ( have_posts() ) :
+
+				while ( have_posts() ) :
+					the_post();
+					//echo get_the_title();
+					get_template_part( 'template-parts/content', 'blog' );
+				endwhile;
+			endif;
+
+			wp_reset_postdata();
+
+			global $wp_query;
+			$big = 999999999; // need an unlikely integer.
+
+			$page_args = array(
+				'base'      => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+				'format'    => '?paged=%#%',
+				'current'   => max( 1, get_query_var( 'paged' ) ),
+				'total'     => $wp_query->max_num_pages,
+				'prev_next' => true,
+				'show_all'  => false,
+				'prev_text' => __( '<span class="dashicons dashicons-arrow-left"></span>' ),
+				'next_text' => __( '<span class="dashicons dashicons-arrow-right"></span>' ),
+			);
+
+			?>
+
+			<div class="blog-pagination">
+				<?php
+				echo paginate_links( $page_args );
+				?>
+			</div>
+		</div>
+
+		<div class="sidebar-widget">
+		<?php
+			if ( is_active_sidebar( 'yohe-1' ) ) :
+				dynamic_sidebar( 'yohe-1' );
+			endif;
+		?>
+		</div>
+	</main>
+</div>
+
+<?php
+get_footer();
