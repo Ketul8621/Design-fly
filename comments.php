54c9b10209f6548a7b24bf09a5512569057a4a57
ketul8621 <ketul.solanki@pay1.in>
2021-10-05 15:07:39 +0530

    Modify the file.

--- comments.php
+++ comments.php
@@ -11,11 +11,11 @@
 <div id="comments" class="comments-area">
 
 	<?php
-		if ( have_comments() ):
-	?>
+	if ( have_comments() ) :
+		?>
 
-	<ol class="comment-list">
-		<?php
+		<ol class="comment-list">
+			<?php
 			$args = array(
 				'max_depth'         => 3,
 				'style'             => 'ol',
@@ -32,45 +32,39 @@
 			);
 
 			wp_list_comments( $args );
+			?>
+		</ol>
+
+		<div class="comment-line"></div>
+
+		<?php
+		if ( ! comments_open() && get_comments_number() ) :
+			?>
+			<p class="no-comments"><?php esc_html_e( 'Comments are closed', 'design-fly' ); ?></p>
+			<?php
+			endif;
 		?>
-	</ol>
 
-	<div class="comment-line"></div>
-
-	<?php
-		if ( !comments_open() && get_comments_number() ):
-	?>
-		<p class="no-comments"><?php esc_html_e( 'Comments are closed', 'design-fly' ); ?></p>
-	<?php
-		endif;
-	?>
-
-	<?php
-		endif;
+		<?php
+	endif;
 	?>
 
 	<?php
 
-		//$fields = array(
-		//	'author' => '<p class="comment-form-author"><br /><input id="author" name="author" aria-required="true" "></input></p>',
-		//	'email' => '<p class="comment-form-email"><br /><input id="email" name="email" "></input></p>',
-		//	'url' => '<p class="comment-form-url"><br /><input id="url" name="url" "></input></p>',
-		//);
+	$fields = array(
+		'author' => '<div class="comment-fields"><div class="author-field"><label for="author">Name</label><br /><input id="author" name="author" aria-required="true" "></input></div>',
+		'email'  => '<div class="email-field"><label for="email">Email</label><br /><input id="email" name="email" "></input></div>',
+		'url'    => '<div class="url-field"><label for="url">URL</label><br /><input id="url" name="url" "></input></div></div>',
+	);
 
-		$fields = array(
-			'author' => '<div class="comment-fields"><div class="author-field"><label for="author">Name</label><br /><input id="author" name="author" aria-required="true" "></input></div>',
-			'email' => '<div class="email-field"><label for="email">Email</label><br /><input id="email" name="email" "></input></div>',
-			'url' => '<div class="url-field"><label for="url">URL</label><br /><input id="url" name="url" "></input></div></div>',
-		);
-
-		$arg = array(
-			'title_reply' => 'Post your comment',
-			'label_submit' => __('Submit'),
-			'comment_notes_before' => '',
-			'comment_field' => '<p class="comment-form-comment"><textarea id="comment" name="comment" aria-required="true" "></textarea></p>',
-			'fields' => apply_filters( 'commet_form_default_fields', $fields ),
-		);
-		comment_form( $arg );
+	$arg = array(
+		'title_reply'          => 'Post your comment',
+		'label_submit'         => __( 'Submit', 'design-fly' ),
+		'comment_notes_before' => '',
+		'comment_field'        => '<p class="comment-form-comment"><textarea id="comment" name="comment" aria-required="true" "></textarea></p>',
+		'fields'               => apply_filters( 'commet_form_default_fields', $fields ),
+	);
+	comment_form( $arg );
 
 	?>
 </div>
