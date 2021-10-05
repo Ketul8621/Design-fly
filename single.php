863a1ecf70e20c52ec6fd7d4f3e39d637c08acd5
ketul8621 <ketul.solanki@pay1.in>
2021-10-05 15:15:12 +0530

    Add comment section and sidebar section.

--- single.php
+++ single.php
@@ -9,40 +9,67 @@ echo get_the_author();
 get_header();
 ?>
 
-<?php if( have_posts() ): the_post(); ?>
-<div id="single-page" class="single-class" >
-	<main id="main-single" class="site-main" role="main">
-		<div class="blog-body">
+<?php
+if ( have_posts() ) :
+	the_post();
+	df_count_post_views( get_the_ID() );
+	?>
 
-			<!--<div class="title-part">
-				<div class="blog-title-col">
-					<p class="title-phrase">LET'S BLOG</p>
-				</div>
-			</div>-->
-
-			<div class="row">
-				<div class="col-12 blog-column">
-					<p class="single-blog-title"><?php the_title(); ?></p>
-					<div class="meta-info-box">
-						<div class="author-date">by <a href="#"> <?php the_author(); ?></a> on <?php echo get_the_date( "d M Y" ); ?></div>
-						<div class="tot-comments"><a href="#">12 comments</a></div>
+	<div id="single-post" class="single-class" >
+		<main id="main-single" class="site-main" role="main">
+			<div class="blog-body">
+				<div class="row">
+					<div class="col-12 blog-column">
+						<p class="single-blog-title"><?php the_title(); ?></p>
+						<div class="meta-info-box">
+							<div class="author-date">by <?php the_author_posts_link(); ?> on <?php echo get_the_date( "d M Y" ); ?></div>
+							<div class="tot-comments"><a href="#">12 comments</a></div>
+						</div>
 					</div>
 				</div>
-			</div>
 
-			<div class="short-line"></div>
+				<div class="short-line"></div>
 
-			<div class="row single-row-content">
-				<div class="col-12 single-col-content">
-					<?php the_content(); ?>
-					<div class="show-tags"><?php the_tags(); ?></div>
+				<div class="row single-row-content">
+					<div class="col-12 single-col-content">
+						<?php the_content(); ?>
+						<div class="show-tags"><?php the_tags(); ?></div>
+					</div>
 				</div>
+
+				<div class="short-line"></div>
+
+				<div class="row comment-title-row">
+					<div class="col-12 comment-title-col">
+						Comments
+					</div>
+				</div>
+
+				<div class="short-line"></div>
+
+				<div class="row comments-row">
+					<div class="col-12 comments-col">
+						<?php
+						if ( comments_open() ) :
+							comments_template();
+						endif;
+						?>
+					</div>
+				</div>
+
+
 			</div>
 
-		</div>
+			<div class="sidebar-widget">
+				<?php
+				if ( is_active_sidebar( 'yohe-2' ) ) :
+					dynamic_sidebar( 'yohe-2' );
+				endif;
+				?>
+			</div>
 
-	</main>
-</div>
+		</main>
+	</div>
 <?php endif; ?>
 
 <?php
