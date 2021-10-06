445c3b896f101746dfefbfd4cda2006ff87b41df
ketul8621 <ketul.solanki@pay1.in>
2021-10-06 18:48:01 +0530

    Add function to add active class to blog tab and add the required paths.

--- functions.php
+++ functions.php
@@ -7,20 +7,18 @@
 
 require get_template_directory() . '/template-parts/enqueue.php';
 require get_template_directory() . '/template-parts/theme-support.php';
-require get_template_directory() . '/include/walker.php';
 require get_template_directory() . '/template-parts/footbar-widgets.php';
 require get_template_directory() . '/include/photograph.php';
 require get_template_directory() . '/include/multimedia.php';
 require get_template_directory() . '/include/adver.php';
-
-add_theme_support( 'post-thumbnails' );
+require get_template_directory() . '/include/excerpt-trim.php';
 
 /**
- * Add active class to the current menu item
+ * Add 'active' class to the current menu item
  *
  * @param array  $classes Applied classes of an element.
  * @param object $item
- * @return void
+ * @return array $classes
  */
 function df_special_nav_class( $classes, $item ) {
 
@@ -31,39 +29,71 @@ function df_special_nav_class( $classes, $item ) {
 	return $classes;
 }
 
-// Register the special_nav_class with nav_menu_css_class hook.
+// Register the df_special_nav_class with nav_menu_css_class hook.
 add_filter( 'nav_menu_css_class', 'df_special_nav_class', 10, 2 );
 
-function df_my_widget() {
+/**
+ * Add 'active' class to the current menu item
+ *
+ * @param array  $classes Applied classes of an element.
+ * @param object $item
+ * @return array $classes
+ */
+function df_special_class( $classes, $item ) {
 
-	register_sidebar( array(
-		'name'          => 'Sidebar',
-		'id'            => 'yohe-1',
-		'before_widget' => '<div class="chw-widget">',
-        'after_widget'  => '</div>',
-	) );
+	global $wp;
+	$current_url  = home_url( add_query_arg( $_GET,$wp->request ) );
+	$current_post = url_to_postid( $current_url );
+
+	$query = new WP_Query(
+		array(
+			'post_type' => 'post',
+		),
+	);
+
+	if ( $query->have_posts() ) :
+		while ( $query->have_posts() ) :
+			$query->the_post();
+			$id_array[] = get_the_ID();
+		endwhile;
+	endif;
+
+	wp_reset_postdata();
+
+	if ( in_array( 'menu-item-132', $classes ) && in_array( $current_post, $id_array ) ) {
+		$classes[] = 'active';
+	}
+
+	return $classes;
 }
 
-add_action( 'widgets_init', 'df_my_widget' );
+// Register the df_special_class with nav_menu_css_class hook.
+add_filter( 'nav_menu_css_class', 'df_special_class', 10, 2 );
 
+/**
+ * Get the gallery images
+ *
+ * @param object $post_id The post id.
+ * @return array $srcs
+ */
 function get_post_block_galleries_images( $post_id ) {
-    $content = get_post_field( 'post_content', $post_id );
-    $srcs = [];
+	$content = get_post_field( 'post_content', $post_id );
+	$srcs    = [];
 
-    $i = -1;
-    foreach ( parse_blocks( $content ) as $block ) {
-        if ( 'core/gallery' === $block['blockName'] ) {
-            $i++;
-            $srcs[ $i ] = [];
+	$i = -1;
+	foreach ( parse_blocks( $content ) as $block ) {
+		if ( 'core/gallery' === $block['blockName'] ) {
+			$i++;
+			$srcs[ $i ] = [];
 
-            preg_match_all( '#src=([\'"])(.+?)\1#is', $block['innerHTML'], $src, PREG_SET_ORDER );
-            if ( ! empty( $src ) ) {
-                foreach ( $src as $s ) {
-                    $srcs[ $i ][] = $s[2];
-                }
-            }
-        }
-    }
+			preg_match_all( '#src=([\'"])(.+?)\1#is', $block['innerHTML'], $src, PREG_SET_ORDER );
+			if ( ! empty( $src ) ) {
+				foreach ( $src as $s ) {
+					$srcs[ $i ][] = $s[2];
+				}
+			}
+		}
+	}
 
-    return $srcs;
+	return $srcs;
 }
